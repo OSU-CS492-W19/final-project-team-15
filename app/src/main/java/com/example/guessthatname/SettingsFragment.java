4559204177d941301a2ce8b7f20a524f76b1fb93
Brett Case <brett.case97@gmail.com>
2019-02-28 18:44:46 -0800

    adding some settings and colors

---
+++ app/src/main/java/com/example/guessthatname/SettingsFragment.java
@@ -0,0 +1,35 @@
+package com.example.guessthatname;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        CharSequence[] entries = { "One", "Two", "Three" };
+        CharSequence[] entryValues = { "1", "2", "3" };
+        ListPreference lp = (ListPreference)findPreference("genre-key");
+        lp.setEntries(entries);
+        lp.setEntryValues(entryValues);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
