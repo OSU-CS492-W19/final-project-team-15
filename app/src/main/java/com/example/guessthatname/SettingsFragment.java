f293a18ce27b16e5fd9165bd32dcc7e35dee4366
Brett Case <brett.case97@gmail.com>
2019-03-07 17:47:27 -0800

    preferences are now fully dynamic

--- app/src/main/java/com/example/guessthatname/SettingsFragment.java
+++ app/src/main/java/com/example/guessthatname/SettingsFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.util.Log;
+import android.util.Pair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
@@ -12,8 +17,9 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         addPreferencesFromResource(R.xml.prefs);
         ListPreference lp = (ListPreference) findPreference("genre_key");
         Log.d(this.getClass().toString(), lp.toString());
-        lp.setEntries(new String[]{"one","two","three"});
-        lp.setEntryValues(new String[]{"0","1","2"});
+        Pair<ArrayList<String>, ArrayList<String>> Entries = getCategories();
+        lp.setEntries(Entries.first.toArray(new String[0]));
+        lp.setEntryValues(Entries.second.toArray(new String[0]));
         lp.setValueIndex(0);
     }
 
@@ -32,5 +38,16 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     public void onPause() {
         super.onPause();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public Pair<ArrayList<String>, ArrayList<String>> getCategories(){
+        Pair<ArrayList<String>, ArrayList<String>> categories = new Pair<>(new ArrayList<String>(), new ArrayList<String>());
+        categories.first.add("one");
+        categories.first.add("two");
+
+        categories.second.add("1");
+        categories.second.add("2");
+
+        return categories;
     }
 }
